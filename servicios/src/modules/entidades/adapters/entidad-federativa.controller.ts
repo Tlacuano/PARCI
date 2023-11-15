915729bbe953b856da08638d45a2bd9f529c93fe
DMarjar <20213tn119@utez.edu.mx>
2023-11-14 23:34:47 -0600

    [FIXED] Get Entidades Federativas
    Faltaba agregar el router a express.

--- servicios/src/modules/entidades/adapters/entidad-federativa.controller.ts
+++ servicios/src/modules/entidades/adapters/entidad-federativa.controller.ts
@@ -1,7 +1,7 @@
-import Response, Request, { Router } from "express";
+import { Request, Response, Router } from "express";
 import { EntidadFederativaRepository } from "../use-cases/ports/entidad-federativa.repository";
 import { EntidadFederativaStorageGateway } from "./entidad-federativa.storage.gateway";
-import { GetEntidadesFederativas } from './../use-cases/get-entidades-federativas.interactor';
+import { GetEntidadesFederativas } from "./../use-cases/get-entidades-federativas.interactor";
 import { ResponseApi } from "../../../kernel/types";
 import { EntidadFederativa } from "../entities/entidad-federativa";
 import { validarError } from "../../../kernel/error-handler";
@@ -9,26 +9,39 @@ import { validarError } from "../../../kernel/error-handler";
 const entidadesFederativasRouter = Router();
 
 export class EntidadesFederativasController {
-    getEntidadesFederativas = async (req: Request, res: Response) => {
-        try {
-            const repositorio: EntidadFederativaRepository = new EntidadFederativaStorageGateway;
-            const GetEntidadesFederativasInteractor = new GetEntidadesFederativas(repositorio);
+  getEntidadesFederativas = async (_req: Request, res: Response) => {
+    console.log('hola');
+    try {
+        console.log('hola');
+      const repositorio: EntidadFederativaRepository =
+        new EntidadFederativaStorageGateway();
+      const GetEntidadesFederativasInteractor = new GetEntidadesFederativas(
+        repositorio
+      );
 
-            const entidadesFederativas = await GetEntidadesFederativasInteractor.execute();
+      const entidadesFederativas =
+        await GetEntidadesFederativasInteractor.execute();
 
-            const body: ResponseApi<EntidadFederativa[]> = {
-                data: entidadesFederativas,
-                message: 'Entidades federativas obtenidas correctamente',
-                status: 200,
-                error: false
-            }
+      const body: ResponseApi<EntidadFederativa[]> = {
+        data: entidadesFederativas,
+        message: "Entidades federativas obtenidas correctamente",
+        status: 200,
+        error: false,
+      };
 
-            res.status(body.status).json(body);
-
-
-        } catch (error) {
-            const errorBody = validarError(error as Error);
-            res.status(errorBody.status).json(errorBody);
-        }
+      res.status(body.status).json(body);
+    } catch (error) {
+      const errorBody = validarError(error as Error);
+      res.status(errorBody.status).json(errorBody);
     }
+  };
 }
+
+const entidadesFederativasController = new EntidadesFederativasController();
+
+entidadesFederativasRouter.get(
+  "/entidades-federativas",
+  entidadesFederativasController.getEntidadesFederativas
+);
+
+export default entidadesFederativasRouter;
