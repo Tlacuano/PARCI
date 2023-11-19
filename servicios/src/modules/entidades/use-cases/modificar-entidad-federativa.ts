f11bac0a8ffe0f5504cec32f34b92fbe7897be25
DMarjar <20213tn119@utez.edu.mx>
2023-11-18 19:28:52 -0600

    [UPDATE] Validacion de nombre repetido
    Se agrego la validacion para verificar si un nombre de entidad federativa esta repetido

--- servicios/src/modules/entidades/use-cases/modificar-entidad-federativa.ts
+++ servicios/src/modules/entidades/use-cases/modificar-entidad-federativa.ts
@@ -1,12 +1,16 @@
 import { UseCase } from "src/kernel/contracts";
 import { EntidadFederativaRepository } from "./ports/entidad-federativa.repository";
 import { ModificarEntidadFederativaDTO } from "../adapters/dtos/modificar-entidad-federativa.dto";
+import { nombreRepetido } from "../utils/nombre-repetido";
 
 export class ModificarEntidadFederativaInteractor implements UseCase<ModificarEntidadFederativaDTO, boolean> {
   constructor(private readonly repository: EntidadFederativaRepository) {}
 
   async execute(payload: ModificarEntidadFederativaDTO): Promise<boolean> {
     payload.nombre_entidad = payload.nombre_entidad.trim();
+    if (await nombreRepetido(payload.nombre_entidad)) {
+      throw new Error("El nombre ya existe");
+    }
     if (payload.nombre_entidad === "") {
       throw new Error("El nombre es requerido");
     }
